c8222d7b2a65e2c19cdc9f04e57c7fac81219187
Lalith Kumar <lalithkmr94@gmail.com>
2017-09-06 13:22:20 +0530

    Modification - To serve static pages. As well as added 2 static html pages(just for experimental purpose alone) - home.php . It can be accessed with an URL like ‘http://domainname/home' - about.php. It can be accessed with an URL like ‘http://domainname/about'

---
+++ application/views/pages/about.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to ABOUT page</title>
+</head>
+<body>
+    <h1>Working!!!</h1>
+    <h2>I am in ----------About Us ------------------- page</h2>
+    <p> Page url is in : <b>application/views/pages/about.php</b></p>
+</body>
+
+</head>
---
+++ application/views/pages/home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Home page</title>
+</head>
+<body>
+	<h1>Working!!!</h1>
+    <p> Page url is in : <b>application/views/pages/home.php</b></p>
+</body>
+
+</head>
